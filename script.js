01f612a81c26d35f373f87e82b8da6b8f038688e
codepirateAd <anupampro08@gmail.com>
2022-01-23 12:03:40 +0530

    sidebar phone

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
 console.log("sidebarBtn");
 sidebarBtn.addEventListener("click", ()=>{
-// if(document.getElementsByClassName('sidebar')[0].style.display == null);{
-//     document.getElementsByClassName('sidebar')[0].style.display ="block";
-//     document.getElementsByClassName('home-section')[0].style.position ="relative";
-//     document.getElementsByClassName('home-section')[0].style.width ="calc(100%);";
-//     document.getElementsByClassName('home-section')[0].style.width ="calc(100%);";
-//   }
-  sidebar.classList.toggle("close");
+if(document.getElementsByClassName('sidebar')[0].style.display == '' || document.getElementsByClassName('sidebar')[0].style.display == 'none'){
+    document.getElementsByClassName('sidebar')[0].style.display ="block";
+    document.getElementsByClassName('home-section')[0].style.position ="relative";
+    console.log("if...");
+    sidebar.classList.toggle("close");
+
+
+  }
+  else{
+    console.log("else...");
+    document.getElementsByClassName('sidebar')[0].style.display = 'none';
+    document.getElementsByClassName('home-section')[0].style.position ="initial";
+    document.getElementsByClassName('home-section')[0].style.width="100%";
+    sidebar.classList.toggle("close");
+  }
 });
 
 
